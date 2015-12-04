25f0406776fd4a372208b13cf077108cdb869028
Marvin Danig <marvin@marvindanig.com>
2015-12-04 16:43:59 -0500

    Checking event on IE

--- javascripts/script.js
+++ javascripts/script.js
@@ -16,6 +16,7 @@ if (isFirefox) {
   };   
 } else if (isIE) {
   window.onscroll = function() {
+    console.log('yeah');
     if (window.pagYOffset > 80 && !header.classList.contains("nav")) {
       header.classList.add("nav");
     } else if (window.pagYOffset <= 80) {
