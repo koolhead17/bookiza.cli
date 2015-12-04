e4d9079db2d35e6a585f774df3bb61372c0f6a67
Marvin Danig <marvin@marvindanig.com>
2015-12-04 15:36:33 -0500

    Adding cross-browser js to animate header

---
+++ javascripts/script.js
@@ -0,0 +1,34 @@
+var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+var isFirefox = typeof InstallTrigger !== 'undefined'; 
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+var isChrome = !!window.chrome && !isOpera;
+var isIE = /*@cc_on!@*/false || !!document.documentMode; 
+
+
+var header = document.getElementsByTagName("header")[0];
+
+if (isFirefox) {
+  window.onscroll = function() {
+    if (document.documentElement.scrollTop > 80 && !header.classList.contains("nav")) {
+      header.classList.add("nav");
+    } else if (document.documentElement.scrollTop <= 80) {
+      header.classList.remove("nav");
+    }
+  };   
+} else if (isIE) {
+  window.onscroll = function() {
+    if (document.body.scrollTop > 80 && !header.classList.contains("nav")) {
+      header.classList.add("nav");
+    } else if (document.body.scrollTop <= 80) {
+      header.classList.remove("nav");
+    }
+  };   
+} else {
+  window.onscroll = function() {
+    if (document.body.scrollTop > 80 && !header.classList.contains("nav")) {
+      header.classList.add("nav");
+    } else if (document.body.scrollTop <= 80) {
+      header.classList.remove("nav");
+    }
+  };   
+}
